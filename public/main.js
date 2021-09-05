3be4ed00d2efbe2ce88e5477d2e82782e8d4649c
Onur BOSTANCIOĞLU <onur.bostancioglu@gmail.com>
2021-09-06 00:00:26 +0300

    Update main.js

--- public/main.js
+++ public/main.js
@@ -3,7 +3,7 @@
 import { APIWrapper, API_EVENT_TYPE } from "./api.js";
 import { addMessage, animateGift, isPossiblyAnimatingGift, isAnimatingGiftUI } from "./dom_updates.js";
 
-const api = new APIWrapper(null, true, true);
+const api = new APIWrapper();
 
 api.setEventHandler((events) => {
 
